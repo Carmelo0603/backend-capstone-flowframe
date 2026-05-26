1268ea125fd183f05d30eeb03d4ba6e248350113
Carmelo0603 <carmeloprovenzani1@gmail.com>
2026-05-26 12:21:39 +0200

    setup iniziale progetto e alberatura pacchetti

---
+++ src/main/java/it/epicode/backendcapstoneflowframe/BackendCapstoneFlowframeApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.backendcapstoneflowframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCapstoneFlowframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCapstoneFlowframeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/backendcapstoneflowframe/BackendCapstoneFlowframeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.backendcapstoneflowframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCapstoneFlowframeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
